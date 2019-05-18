88f422f18835f6d1013feb0910dc0adf31b8f605
Rodrigo <rodrigobravo-@hotmail.com>
2019-05-18 01:01:35 -0300

    Form en desarrollo

---
+++ EnvioMails/EnvioMails/Mail.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnvioMails
+{
+    public class Mail
+    {
+        public int Orden { get; set; }
+        public string EmailDestino { get; set; }
+        public string Asunto { get; set; }
+        public string Cuerpo { get; set; }
+        public bool Enviado { get; set; }
+        public string Motivo { get; set; }
+        public static string Pass { get; set; }
+        public static string EmailOrigen { get; set; }
+
+        public Mail()
+        {
+
+        }
+        public Mail(string emailDestino, string asunto, string cuerpo)
+        {
+            this.EmailDestino = emailDestino;
+            this.Asunto = asunto;
+            this.Cuerpo = cuerpo;
+        }
+
+        public Mail(string pass, string emailOrigen, string emailDestino, string asunto, string cuerpo) : this(emailDestino, asunto, cuerpo)
+        {
+            Pass = pass;
+            EmailOrigen = emailOrigen;
+        }
+
+        public static void EnviarMail(Mail m)
+        {
+            try
+            {
+                var gmail = EmailOrigen.Contains("gmail");
+                var outlook = EmailOrigen.Contains("outlook") ? true : EmailOrigen.Contains("hotmail") ? true : false;
+                MailMessage objeto_mail = new MailMessage();
+                SmtpClient client = new SmtpClient();
+                client.Port = 25;
+                /*
+                    Nombre de servidor: smtp.office365.com
+                    Puerto: 587
+                    Método de cifrado: STARTTLS
+                 */
+                client.Host = gmail ? "smtp.gmail.com" : outlook ? "smtp.office365.com" : throw new Exception("Mail incorrecto");
+                client.Timeout = 10000;
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.EnableSsl = true;
+                client.Credentials = new System.Net.NetworkCredential(EmailOrigen, Pass);
+                objeto_mail.From = new MailAddress(EmailOrigen);
+                objeto_mail.To.Add(new MailAddress(m.EmailDestino));
+                objeto_mail.Subject = m.Asunto;
+                objeto_mail.Body = m.Cuerpo;
+                client.Send(objeto_mail);
+                m.Enviado = true;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Contraseña inválida");
+            }
+        }
+        public bool ValidarMail()
+        {
+            try
+            {
+                var addr = new MailAddress(this.EmailDestino);
+                return addr.Address == this.EmailDestino;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ EnvioMails/PruebaConsola/Program.cs
@@ -0,0 +1,47 @@
+﻿using EnvioMails;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaConsola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Ingrese cuenta: ");
+                var origen = Console.ReadLine();
+                Console.Write("Password: ");
+                var pass = Console.ReadLine();
+                Console.Write("Ingrese asunto: ");
+                var asunto = Console.ReadLine();
+                Console.Write("Ingrese: ");
+                var cuerpo = Console.ReadLine();
+
+                var listaContactos = new List<string>();
+                //listaContactos.Add("rodrigoebravo@hotmail.com");
+                //listaContactos.Add("rodrigoebravo@hotmail.com");
+                //listaContactos.Add("rodrigoebravo07@gmail.com");
+                listaContactos.Add("eliorodrigobravo@gmail.com");
+
+                foreach (var mail in listaContactos)
+                {
+                    var m = new Mail(pass, origen, mail, asunto, cuerpo);
+                    m.ValidarMail();
+                    Mail.EnviarMail(m);
+                    Console.WriteLine("OK");
+                }
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+            }
+        }
+    }
+}
